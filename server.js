c576c18fc5e85823ae36d1716db7c59e2a828b41
Lee Kin Fei <kinfei.lee@qumonintelligence.com>
2021-11-12 22:46:29 +0800

    Init

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const app = express();
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/tutorials");
+
+mongoose.connection.once("open", () => {
+  console.log("conneted to database");
+});
+
+//This route will be used as an endpoint to interact with Graphql,
+//All queries will go through this route.
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    //Directing express-graphql to use this schema to map out the graph
+    schema,
+    //Directing express-graphql to use graphiql when goto '/graphql' address in the browser
+    //which provides an interface to make GraphQl queries
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
